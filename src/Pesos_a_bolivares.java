f6b81fec5de1da4c2a328b35303ee123c688acc6
Juan Esteban Florez Perlaza <esteban200506@gmail.com>
2022-06-18 09:10:48 -0500

    en el main se hizo un software de transformacion de pesos a bolivares

--- src/Pesos_a_bolivares.java
+++ src/Pesos_a_bolivares.java
@@ -1,6 +1,11 @@
+import javax.swing.*;
+
 public class Pesos_a_bolivares {
 
     public static void main(String[] args) {
 
+        int pesos= Integer.parseInt(JOptionPane.showInputDialog("ingrese la cantidad de pesos a transformar"));
+        double bolivares = pesos*0.0014;
+        JOptionPane.showMessageDialog(null, "El total de bolivares es de " +bolivares);
     }
 }
